b716bac613ee81fc581eb70b634e23624090a039
Bharadwaja-rao-D <cs20btech11012@iith.ac.in>
2022-07-01 17:24:51 +0530

    small changes

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,14 +1,45 @@
-use std::{sync::Arc, collections::HashMap};
+use std::{collections::HashMap, sync::Arc};
 
-use rutrack::{db::Users, message::MessageType};
+use futures::StreamExt;
 use futures_channel::mpsc::UnboundedSender;
+use log::info;
+use rutrack::{error::RutrackError, message::MessageType, opts::Opts};
+use structopt::StructOpt;
+use tokio::net::{TcpListener, TcpStream};
 
 type Tx = UnboundedSender<MessageType>;
 
-pub type Clients = Arc<HashMap<Users, Tx>>;
+//TODO: Pool of Tx ?
+pub type Clients = Arc<HashMap<String, Tx>>;
 
-#[tokio::main]
-async fn main(){
-    let clients: Clients = Arc::new(HashMap::new());
+pub async fn handle_connection(stream: TcpStream) -> Result<(), RutrackError> {
+    //upgrade it to the websocket
+
+    let ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("websocket handshake failed");
+
+    info!("websocket handshake done");
+
+    let (outgoing, incoming) = ws_stream.split();
+
+    return Ok(());
 }
 
+#[tokio::main]
+async fn main() -> Result<(), RutrackError> {
+    let opts = Opts::from_args();
+
+    let _clients: Clients = Arc::new(HashMap::new());
+
+    let listener = TcpListener::bind(opts.address)
+        .await
+        .expect("Binding failed");
+
+    while let Ok((stream, addr)) = listener.accept().await {
+        info!("Connected to {}", addr);
+        handle_connection(stream).await?;
+    }
+
+    return Ok(());
+}
